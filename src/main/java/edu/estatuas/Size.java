150543e939494eab36b0aca10730782527398367
Hugo Gonzalez Besada <hugogbmahu@gmail.com>
2025-03-21 14:15:41 +0100

    feat(Size): crear la clase Size, inicializando, creando el metodo to string . Esta clase filtra por size en el metodo checkCriteria

---
+++ src/main/java/edu/estatuas/Size.java
@@ -0,0 +1,35 @@
+package edu.estatuas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Size implements Criteria {
+
+    private String size;
+    private List<Offer> sizes;
+
+
+    public Size(String size) {
+        this.size = size;
+        this.sizes = new ArrayList<>();
+    }
+
+    private String getSize() {
+        return this.size;
+    }
+
+    @Override
+    public List<Offer> checkCriteria(Item sneaker){
+        for (Offer offer : sneaker.offers()) {
+            if (offer.getSize() == this.size){
+            this.sizes.add(offer);
+            }
+        } return this.sizes;
+    }
+
+    @Override
+    public String toString() {
+        return "" + getSize();
+    }
+
+}
